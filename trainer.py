abd5c7ace31eeaf947768854d84f0d25142d4445
mkutsovsky <mkutsovsky@users.noreply.github.com>
2020-07-30 10:21:56 -0400

    reset baseline

--- trainer.py
+++ trainer.py
@@ -50,7 +50,7 @@ def train_model(cfg: DictConfig):
     data = ImageDataBunch.from_name_re(cfg.dataset.file, fnames, pat, ds_tfms=get_transforms(), size=24, bs=bs).normalize(imagenet_stats)
     log.info(data)
     #print(data.shape)
-    learn = create_cnn(data, models.resnet50, metrics=error_rate)
+    learn = create_cnn(data, models.resnet34, metrics=error_rate)
     #learn = create_cnn(data, models.resnet50, metrics=error_rate)
     
     learn.lr_find()
@@ -122,7 +122,7 @@ def my_app(cfg : DictConfig) -> None:
     validate_data(cfg.dataset)
     
     train_model(cfg)
-    export_to_open_vino(cfg)
+    #export_to_open_vino(cfg)
     
     #deploy_as_endpoint(cfg)
     #make_queries(cfg)
